ff5acfc8e7e5f96af42319f11e674e400e81b795
mpart90 <mp6lgo@gmail.com>
2019-04-28 17:22:38 -0600

    Created welcome bar

---
+++ Welcomestring.php
@@ -0,0 +1,22 @@
+<!Doctype>
+<html lang="en">
+    <head>
+<meta charset="utf-10">
+<title>Home</title>
+<style>
+header {
+    background: #e3e3e3;
+    padding: 3em;
+    text-align: center;
+}
+</style>
+    </head>
+    <body>
+        <header>
+            <h1>
+                <?= "Welcome, " . htmlspecialchars($_GET['name']);?>
+            </h1>
+        </header>
+
+    </body>
+</html>
