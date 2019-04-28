a90de68e2b755b8315996db8cf90809de9b6ad44
mpart90 <mp6lgo@gmail.com>
2019-04-28 09:29:31 -0600

    addining initial files

---
+++ apparel-array.php
@@ -0,0 +1,26 @@
+<!Doctype>
+<html>
+    <head>
+        <title>Types of apparel</title>
+    </head>
+    <body>
+        <?php ?>
+        <?php
+        $apparel=array(Gloves, Jackets, Shirts, Pants, Outerwear, Boots, Helmets);
+        print_r($apparel);
+        ?> <hr>
+        <?php
+        echo current($apparel)."<br>";
+        Next($apparel);
+        echo current($apparel)."<br>";
+        Next($apparel);
+        Next($apparel);
+        echo current($apparel)."<br>";
+        reset($apparel);
+        end($apparel);
+        echo current($apparel)."<br>";
+
+        ?>
+
+    </body>
+</html>
